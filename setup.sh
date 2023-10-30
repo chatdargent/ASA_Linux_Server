6a599a92c19dd6f6885a8aac2edf5e86357c64c3
chatdargent <84394015+chatdargent@users.noreply.github.com>
2023-10-30 17:08:34 -0500

    uncomment steamcmd

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ FILE_NAME=$PROTON_GE_VAR-$PROTON_GE_VER-$PROTON_GE_ARCH
 wget https://github.com/GloriousEggroll/wine-ge-custom/releases/download/$PROTON_GE_VER/$FILE_NAME.tar.xz
 tar -xvf $FILE_NAME.tar.xz
 mv lutris-$PROTON_GE_VER-$PROTON_GE_ARCH ./proton
-#steamcmd +force_install_dir $PWD/server +login anonymous +app_update 2430930 +quit
+steamcmd +force_install_dir $PWD/server +login anonymous +app_update 2430930 +quit
